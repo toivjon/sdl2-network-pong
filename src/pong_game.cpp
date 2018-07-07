d64af4d538d4b59dcf05ffd96a23adb50d2b54da
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-07-07 21:15:11 +0300

    Added a stub how to handle command line arguments.

--- src/pong_game.cpp
+++ src/pong_game.cpp
@@ -1,16 +1,22 @@
 #include "pong_game.h"
 
 #include <iostream>
+#include <iterator>
 #include <SDL/SDL.h>
+#include <string>
+#include <vector>
 
 using namespace pong;
 
 Game::Game(int argc, char** argv)
 {
-  // TODO ... handle command line arguments ...
-  std::cout << "Hello Pong: " << argc << " -- " << argv << std::endl;
-
   SDL_Log("Constructing a Game instance....");
+
+  // process the provided command line arguments.
+  for (int i = 1; i < argc; i++) {
+    // ... handle?
+    std::string arg(argv[i]);
+  }
 
   // initialize SDL along with the subsystems.
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
