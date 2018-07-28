c85e865926048d5687c3951af4a2ca0eb3f26215
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-07-28 13:21:06 +0300

    Process SDL initialization and destruction with the RAII principle.

--- src/game.cpp
+++ src/game.cpp
@@ -1,18 +1,25 @@
 #include "game.h"
 
 #include <cstdio>
+#include <SDL/SDL.h>
+#include <stdexcept>
 
 using namespace pong;
 
 Game::Game(int argc, char* argv[])
 {
   printf("%d %s", argc, argv[0]);
-  // ...
+
+  // initialize SDL along with all subsystems.
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    printf("SDL_Init: %s\n", SDL_GetError());
+    throw std::runtime_error("Unable to initialize SDL.");
+  }
 }
 
 Game::~Game()
 {
-  // ...
+  SDL_Quit();
 }
 
 void Game::run()
