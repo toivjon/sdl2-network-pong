0e361e29f0d1feed66d649c99e40819f72ccff44
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-07-28 21:07:03 +0300

    Cleanup...

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,4 @@
-#include "game.h"
-
-#include <cstdio>
-#include <exception>
-
-using namespace pong;
 
 int main(int argc, char* argv[]) {
-  try {
-    Game game(argc, argv);
-    game.run();
-  } catch (const std::exception& e) {
-    printf("Fatal error: %s\n", e.what());
-  }
-  return 0;
+  // TODO ...
 }
