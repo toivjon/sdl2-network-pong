f5848bd532deb28d8362aed60c8fa09dc9c0aefd
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-07-28 13:18:57 +0300

    Added a new Game instance that acts as the cental point of the application.

---
+++ src/game.cpp
@@ -0,0 +1,21 @@
+#include "game.h"
+
+#include <cstdio>
+
+using namespace pong;
+
+Game::Game(int argc, char* argv[])
+{
+  printf("%d %s", argc, argv[0]);
+  // ...
+}
+
+Game::~Game()
+{
+  // ...
+}
+
+void Game::run()
+{
+  // ...
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
+#include "game.h"
+
+#include <cstdio>
+#include <exception>
+
+using namespace pong;
+
 int main(int argc, char* argv[]) {
+  try {
+    Game game(argc, argv);
+    game.run();
+  } catch (const std::exception& e) {
+    printf("Fatal error: %s\n", e.what());
+  }
   return 0;
 }
